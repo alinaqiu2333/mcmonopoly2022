2a83287600a8b9b80f41c1ece0f79cb17dbd8a34
alinaqiu2333 <alinaqiu2333@gmail.com>
2022-10-13 19:30:29 -0400

    frontend added

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
 from cgitb import html
 from flask import Flask, render_template, url_for, request
-
+from mysql.connector import (connection)
+import mysql.connector
+from mysql.connector import errorcode
 app = Flask(__name__)
+
+try:
+    cnx = mysql.connector.connect(user='root', password='Alina1116@',
+                                host='127.0.0.1',
+                                database='mcd_mono',auth_plugin='mysql_native_password')
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Something is wrong with your user name or password")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("Database does not exist")
+    else:
+        print(err)
+else:
+    print("all is well")
+    cnx.close()
+
+
 
 def boo():
     d = {}
     for i in range(10):
         d[i] = str(i)*10
     return '这些图片为什么出不来呜呜呜'
-
 
 
 @app.route("/")
---
+++ mcd.sql
@@ -0,0 +1,6 @@
+drop table Own;
+CREATE TABLE Own (
+	ID int NOT NULL,
+	Color varchar(255) NOT NULL,
+	primary key (ID)
+);
